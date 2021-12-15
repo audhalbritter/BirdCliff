ec207b0e4a54eb9b7b639ecbc4342739b9c445bc
audhalbritter <me@audhalbritter.com>
2021-12-15 16:21:49 +0100

    comment out histogram

--- R/analysis_plan.R
+++ R/analysis_plan.R
@@ -1,19 +1,11 @@
 #
 analysis_plan <- list(
   # do something interesting
-  # calculate mean trait value
-  tar_target(
-    name = trait_mean,
-    command = traits_gradient %>%
-      group_by(Gradient, Trait) %>%
-      summarise(Mean = mean(Value)) %>%
-      pivot_wider(names_from = Gradient, values_from = Mean)
-    ),
 
   # diversity
   tar_target(
     name = diversity_grad,
-    command = comm_gradient %>%
+    command = comm_raw %>%
     group_by(Gradient, Site, PlotID, Elevation_m) %>%
     summarise(Richness = n(),
               Diversity = diversity(Cover),
