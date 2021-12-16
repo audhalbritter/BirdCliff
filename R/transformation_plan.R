e46293f011a615cba43ea494dfc121e941747d06
audhalbritter <me@audhalbritter.com>
2021-12-16 08:54:26 +0100

    log transform growth variables import coordinates remove duplicates in comm data

--- R/transformation_plan.R
+++ R/transformation_plan.R
@@ -2,16 +2,47 @@
 # With many intermediate steps, it uses a lot of disk space.
 
 transformation_plan <- list(
+
+  # import coordinates
+  tar_target(
+    name = coordinates,
+    command = read_excel(path = "clean_data/PFTC4_Svalbard_Coordinates.xlsx") %>%
+      filter(Project == "T", Treatment %in% c("C", "B"), !Site %in% c("CAS", "BIS", "DRY")) %>%
+      mutate(Site = as.numeric(Site)) %>%
+      select(Gradient = Treatment, Site:Longitude_E)
+  ),
+
   # import traits
   tar_target(
     name = traits_raw,
-    command = read_csv(file = "clean_data/traits/PFTC4_Svalbard_2018_Gradient_Traits.csv")
+    command = read_csv(file = "clean_data/traits/PFTC4_Svalbard_2018_Gradient_Traits.csv") %>%
+      #log transform size and area traits
+      mutate(
+        value_trans = if_else(
+          Trait %in% c(
+            "Plant_Height_cm",
+            "Dry_Mass_g",
+            "Leaf_Area_cm2",
+            "Thickness_mm"
+          ),
+          true = suppressWarnings(log(Value)),# suppress warnings from log(-value) in isotopes (these are calculated but not kept)
+          false = Value
+        ),
+        trait_trans = recode(
+          Trait,
+          "Plant_Height_cm" = "Plant_Height_cm_log",
+          "Dry_Mass_g" = "Dry_Mass_g_log",
+          "Leaf_Area_cm2" = "Leaf_Area_cm2_log",
+          "Thickness_mm" = "Thickness_mm_log"
+        )) %>% distinct(trait_trans)
   ),
 
   # import community
   tar_target(
     name = comm_raw,
-    command = read_csv(file = "clean_data/community/PFTC4_Svalbard_2018_Community_Gradient.csv")
+    command = read_csv(file = "clean_data/community/PFTC4_Svalbard_2018_Community_Gradient.csv") %>%
+      # remove duplicates
+      distinct()
   ),
 
   # import climate
@@ -23,7 +54,9 @@ transformation_plan <- list(
   # make bootstrap
   tar_target(
     name = trait_mean,
-    command = make_bootstrapping(comm_raw, traits_raw)
+    command = make_bootstrapping(comm_raw, traits_raw)  %>%
+      left_join(coordinates, by = c("Gradient", "Site"))
+
   )
 
 )
