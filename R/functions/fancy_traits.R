c3ebd91d3c6858b4bed006e91fda973c5e343405
audhalbritter <me@audhalbritter.com>
2021-12-16 08:56:01 +0100

    add function for fancy trait names

---
+++ R/functions/fancy_traits.R
@@ -0,0 +1,23 @@
+#rename traits to fancy names for figures
+#rename traits to fancy names for figures
+
+fancy_trait_name_dictionary <- function(dat){
+
+  dat <- dat %>%
+    mutate(trait_fancy = recode(trait_trans,
+                              C_percent = "C %",
+                              CN_ratio = "CN",
+                              dC13_permil = "dC13 ‰",
+                              dN15_permil = "dN15 ‰",
+                              Plant_Height_cm_log = "Height cm",
+                              Dry_Mass_g_log = "Dry mass g",
+                              LDMC = "LDMC",
+                              Leaf_Area_cm2_log = "Area cm2",
+                              N_percent = "N %",
+                              NP_ratio = "NP",
+                              P_percent = "P %",
+                              SLA_cm2_g = "SLA cm2/g",
+                              Thickness_mm_log = "Thickness mm"))
+
+  return(dat)
+}
