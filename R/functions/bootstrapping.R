57c5e659155c36e69fcd5c57f46d996b6a32d2ce
audhalbritter <me@audhalbritter.com>
2021-12-17 14:38:15 +0100

    can remove excluding filter bryo traits add intraspecific trait calc

--- R/functions/bootstrapping.R
+++ R/functions/bootstrapping.R
@@ -8,21 +8,16 @@ make_bootstrapping <- function(comm_raw, traits_raw){
 
   #prepare trait data
   trait <- traits_raw %>%
-    # remove bryophyte traits
-    filter(! Trait %in% c("Shoot_Length_cm", "Shoot_Length_Green_cm")) %>%
-    select(Gradient, Site, PlotID, Taxon, Trait, Value, trait_trans, value_trans) %>%
-    filter(Trait != "Wet_Mass_g")
+    select(Gradient, Site, PlotID, Taxon, trait_trans, value_trans)
 
   #prepare trait data without intraspecific variation
-  # trait.null <- trait_raw %>%
-  #   # remove bryophyte traits
-  #   filter(! Trait %in% c("Shoot_Length_cm", "Shoot_Length_Green_cm")) %>%
-  #   select(Treatment, Site, PlotID, Taxon, Trait, Value) %>%
-  #   filter(Trait != "Wet_Mass_g") %>%
-  #   group_by(Taxon, Trait) %>%
-  #   summarize(Value = mean(as.numeric(Value), na.rm = T)) %>%
-  #   right_join(trait, by = c("Taxon", "Trait")) %>%
-  #   select(-Value.y, "Value" = Value.x)
+  trait.null <- traits_raw %>%
+    select(Gradient, Site, PlotID, Taxon, trait_trans, value_trans) %>%
+    # do we want to have fixed traits per gradient or across all the gradients???
+    group_by(Taxon, trait_trans) %>%
+    summarize(value_trans = mean(as.numeric(value_trans), na.rm = T)) %>%
+    right_join(trait, by = c("Taxon", "trait_trans")) %>%
+    select(-value_trans.y, "value_trans" = value_trans.x)
 
   #set seed for bootstrapping repeatability
   set.seed(2525)
@@ -37,16 +32,16 @@ make_bootstrapping <- function(comm_raw, traits_raw){
                             min_n_in_sample = 2
   )
 
-  # trait_imp_null <- trait_impute(comm = comm,
-  #                                traits = trait.null,
-  #                                scale_hierarchy = c("Site", "Site_trt", "PlotID"),
-  #                                global = F,
-  #                                taxon_col = "Taxon",
-  #                                trait_col = "Trait",
-  #                                value_col = "Value",
-  #                                abundance_col = "Cover",
-  #                                min_n_in_sample = 2
-  # )
+  trait_imp_null <- trait_impute(comm = comm,
+                                 traits = trait.null,
+                                 scale_hierarchy = c("Gradient", "Site", "PlotID"),
+                                 global = F,
+                                 taxon_col = "Taxon",
+                                 trait_col = "trait_trans",
+                                 value_col = "value_trans",
+                                 abundance_col = "Cover",
+                                 min_n_in_sample = 2
+  )
 
   #check trait coverage
   trait_imp %>%
@@ -54,27 +49,26 @@ make_bootstrapping <- function(comm_raw, traits_raw){
     autoplot(.) +
     theme(axis.text.x = element_text(angle = 90))
 
-  # trait_imp_null %>%
-  #   autoplot(.) +
-  #   theme(axis.text.x = element_text(angle = 90))
+  trait_imp_null %>%
+    autoplot(.) +
+    theme(axis.text.x = element_text(angle = 90))
 
   #do the bootstrapping
   CWM <- trait_np_bootstrap(trait_imp, nrep = 100, sample_size = 200)
-  #CWM_notiv <- trait_np_bootstrap(trait_imp_null, nrep = 100, sample_size = 200)
+  CWM_notiv <- trait_np_bootstrap(trait_imp_null, nrep = 100, sample_size = 200)
 
   CWM_mean <- trait_summarise_boot_moments(CWM) %>%
-    select(Gradient:ci_high_mean, -n)
+    select(Gradient:mean, -n)
 
-  # CWM_notiv_mean <- trait_summarise_boot_moments(CWM_notiv) %>%
-  #   select(Site:mean) %>%
-  #   rename("mean_noitv" = "mean")
+  CWM_notiv_mean <- trait_summarise_boot_moments(CWM_notiv) %>%
+    select(Gradient:mean, -n) %>%
+    rename("mean_noitv" = "mean")
 
-  # traitMean <- CWM_mean %>%
-  #   left_join(CWM_notiv_mean) %>%
-  #   select(-n)
+  traitMean <- CWM_mean %>%
+    left_join(CWM_notiv_mean, by = c("Gradient", "Site", "PlotID", "trait_trans"))
 
   #prepare bootstrapped trait data for analyses
-  traitMean <- CWM_mean %>%
+  traitMean <- traitMean %>%
     ungroup() %>%
     mutate(trait_trans = factor(trait_trans, levels = c("Plant_Height_cm_log", "Dry_Mass_g_log", "Leaf_Area_cm2_log", "Thickness_mm_log", "SLA_cm2_g", "LDMC", "C_percent", "N_percent", "CN_ratio", "P_percent", "NP_ratio", "dC13_permil", "dN15_permil")))
 
