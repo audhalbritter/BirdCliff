4e229ddb4ff2d1250b46d7ae469b07fd11c0db4e
audhalbritter <me@audhalbritter.com>
2022-02-15 16:09:56 +0100

    add correlation among traits

--- R/figures_plan.R
+++ R/figures_plan.R
@@ -16,6 +16,71 @@ figures_plan <- list(
         theme_minimal()
       }),
 
+  # correlation plot
+  tar_target(
+    name = correlation_plot,
+    command = {
+      # trait correlation both gradients
+      trait_corr_all = get_trait_correlations(trait_mean) %>%
+        mutate(r = if_else(r == 1, NA_real_, r)) %>%
+        ggplot(aes(x = trait1, y = trait2, fill = r, label = round(r_sig, 2))) +
+        geom_tile() +
+        labs(x = NULL, y = NULL,
+             title = "Both gradients",
+             fill = "Pearson's\nCorrelation") +
+        scale_fill_gradient2(mid = "#f7f7f7", low = "#f1a340",
+                             high = "#998ec3",
+                             na.value = "grey80",
+                             limits = c(-1, 1)) +
+        geom_text() +
+        theme_minimal() +
+        theme(legend.position = "top") +
+        scale_x_discrete(expand=c(0,0)) +
+        scale_y_discrete(expand=c(0,0))
+
+      # Bird cliff
+      trait_corr_B = get_trait_correlations(trait_mean %>%
+                                                   filter(Gradient == "B")) %>%
+        mutate(r = if_else(r == 1, NA_real_, r)) %>%
+        ggplot(aes(x = trait1, y = trait2, fill = r, label = round(r_sig, 2))) +
+        geom_tile() +
+        labs(x = NULL, y = NULL,
+             title = "Bird cliff",
+             fill = "Pearson's\nCorrelation") +
+        scale_fill_gradient2(mid = "#f7f7f7", low = "#f1a340",
+                             high = "#998ec3",
+                             na.value = "grey80",
+                             limits = c(-1, 1)) +
+        geom_text() +
+        theme_minimal() +
+        theme(legend.position = "top") +
+        scale_x_discrete(expand=c(0,0)) +
+        scale_y_discrete(expand=c(0,0))
+
+      # Control
+      trait_corr_C = get_trait_correlations(trait_mean %>%
+                                              filter(Gradient == "C")) %>%
+        mutate(r = if_else(r == 1, NA_real_, r)) %>%
+        ggplot(aes(x = trait1, y = trait2, fill = r, label = round(r_sig, 2))) +
+        geom_tile() +
+        labs(x = NULL, y = NULL,
+             title = "Bird cliff",
+             fill = "Pearson's\nCorrelation") +
+        scale_fill_gradient2(mid = "#f7f7f7", low = "#f1a340",
+                             high = "#998ec3",
+                             na.value = "grey80",
+                             limits = c(-1, 1)) +
+        geom_text() +
+        theme_minimal() +
+        theme(legend.position = "top") +
+        scale_x_discrete(expand=c(0,0)) +
+        scale_y_discrete(expand=c(0,0))
+
+      trait_corr = trait_corr_all / trait_corr_B / trait_corr_C
+    }),
+
+
+
   # trait change along gradients
   tar_target(
     name = trait_plot,
