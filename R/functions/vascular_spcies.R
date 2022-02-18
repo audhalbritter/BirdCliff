93a277ec577d6717e5968a5a5dec9d70a77f7e13
audhalbritter <me@audhalbritter.com>
2022-02-18 16:55:16 +0100

    ind species analysis

--- R/functions/vascular_spcies.R
+++ R/functions/vascular_spcies.R
@@ -13,7 +13,7 @@
 #   count()
 
 
-make_ind_sp_plot <- function(traits_raw, bryo_traits_raw){
+combine_traits <- function(traits_raw, bryo_traits_raw){
 
   # cobmine species vascular and bryophytes
   all_traits <- bind_rows(traits_raw %>%
@@ -21,10 +21,15 @@ make_ind_sp_plot <- function(traits_raw, bryo_traits_raw){
                                    trait_trans %in% c("Plant_Height_cm_log", "Leaf_Area_cm2_log", "LDMC", "N_percent", "dN15_permil")),
                           bryo_traits_raw %>%
                             filter(Taxon %in% c("aulacomnium turgidum", "hylocomium splendens", "sanionia uncinata"),
-                                   trait_trans %in% c("Shoot_Length_cm_log", "Shoot_Length_Green_cm_log", "Shoot_ratio", "WHC_g_g", "P_percent"))) %>%
-    mutate(trait_trans = factor(trait_trans, levels = c("Plant_Height_cm_log", "Leaf_Area_cm2_log", "LDMC", "N_percent", "dN15_permil", "Shoot_Length_cm_log", "Shoot_Length_Green_cm_log", "Shoot_ratio", "WHC_g_g", "P_percent")),
-           Taxon = factor(Taxon, levels = c("cerastium arcticum", "luzula confusa", "salix polaris", "aulacomnium turgidum", "hylocomium splendens", "sanionia uncinata")))
+                                   trait_trans %in% c("Shoot_Length_cm_log", "Shoot_ratio", "SSL_cm_g", "WHC_g_g", "P_percent"))) %>%
+    mutate(trait_trans = factor(trait_trans, levels = c("Plant_Height_cm_log", "Leaf_Area_cm2_log", "LDMC", "N_percent", "dN15_permil", "Shoot_Length_cm_log", "Shoot_ratio", "SSL_cm_g", "WHC_g_g", "P_percent")),
+           Taxon = factor(Taxon, levels = c("cerastium arcticum", "luzula confusa", "salix polaris", "aulacomnium turgidum", "hylocomium splendens", "sanionia uncinata"))) %>%
+    mutate(GS = paste0(Gradient, Site))
 
+  return(all_traits)
+}
+
+make_ind_sp_plot <- function(all_traits){
 
   v <- fancy_trait_name_dictionary(all_traits) %>%
     filter(Functional_group == "vascular") %>%
@@ -56,3 +61,64 @@ make_ind_sp_plot <- function(traits_raw, bryo_traits_raw){
   return(ind_sp_traits)
 
 }
+
+
+
+# cerastium arcticum 1 dN15_permil
+# salix polaris
+# luzula confusa
+# sanionia uncinata   3 P shoot length shoot ratio
+# aulacomnium turgidum 3 SSL Shoot ratio shoot length
+# hylocomium splendens 2 shoot ratio WHC
+#
+#
+# dd <- all_traits %>%
+#   filter(trait_trans == "Plant_Height_cm_log",
+#          Taxon == "salix polaris")
+# fit <- lmer(value_trans ~ Gradient * Elevation_m + (1|GS), data = dd)
+# fit <- lmer(value_trans ~ Gradient * Elevation_m + (1|GS), na.action = "na.fail", data = dd)
+# model.set = dredge(fit, rank = "AICc", extra = "R^2")
+#
+#
+# all_traits %>% distinct(Taxon)
+#
+# all_traits %>%
+#   group_by(Taxon, trait_trans) %>%
+#   nest(data = -c(Taxon, trait_trans)) %>%
+#   mutate(mod = map(data, ~{
+#     mod <- lmer(value_trans ~ Gradient * Elevation_m + (1|GS), data =  .x)
+#   }))
+#
+#
+# all_traits %>%
+#   filter(Taxon == "luzula confusa") %>%
+#   group_by(trait_trans) %>%
+#   nest(data = -c(trait_trans)) %>%
+#   mutate(model.set = map(data, ~{
+#     mod <- lmer(mean ~  Gradient * Elevation_m + (1|GS), REML = FALSE, na.action = "na.fail", data = .x)
+#     #model.set = dredge(mod, rank = "AICc", extra = "R^2")
+#   })) %>%
+#   unnest(model.set)
+#
+#
+#
+#
+# all_traits %>%
+#   filter(Taxon == "luzula confusa") %>%
+#   group_by(trait_trans) %>%
+#   nest(data = -c(trait_trans)) %>%
+#   mutate(mod = map(data, ~{
+#     mod <- lmer(mean ~  Gradient * Elevation_m + (1|GS), data = .x)
+#   }))
+#
+#
+# all_traits %>%
+#   filter(Taxon == "luzula confusa") %>%
+#   group_by(trait_trans) %>%
+#   nest(data = -c(trait_trans)) %>%
+#   mutate(estimate = map(data, ~{
+#     mod <- lmer(mean ~ Gradient + (1|Site), data = .x)
+#     estimates = broom.mixed::tidy(mod)
+#   })) %>%
+#   unnest(estimate)
+#
