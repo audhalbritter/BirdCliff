e4995892c687000945b4ac2a90f4d6ce304b83bf
audhalbritter <me@audhalbritter.com>
2022-02-18 14:32:45 +0100

    add trait regression analysis

--- R/analysis_plan.R
+++ R/analysis_plan.R
@@ -1,15 +1,16 @@
 #
 analysis_plan <- list(
-  # do something interesting
 
+  # COMMUNITY
   # test diversity along gradients
   tar_target(
     name = diversity_analysis,
     command = {
       diversity_grad %>%
+        mutate(GS = paste0(Gradient, Site)) %>%
         group_by(DiversityIndex) %>%
         nest(data = -c(DiversityIndex)) %>%
-        mutate(mod = map(data, ~lm(Value ~ Gradient * Elevation_m, data = .x)),
+        mutate(mod = map(data, ~lmer(Value ~ Gradient * Elevation_m + (1|GS), data = .x)),
                result = map(mod, tidy)) %>%
           unnest(result)
     }),
@@ -33,6 +34,38 @@ analysis_plan <- list(
         unnest(result)
     }),
 
+
+  # TRAITS
+  # model selection
+  tar_target(
+    name = model_selection,
+    command = make_trait_model_selection(trait_mean)
+  ),
+
+  # Best model: likelihoot ratio test result
+  tar_target(
+    name = best_model,
+    command = {
+      trait_mean %>%
+        distinct(trait_trans) %>%
+        filter(trait_trans != "dC13_permil") %>%
+        mutate(model = c("", "G", "GxE", "", "GxE", "G+E", "G", "", "", "E", "GxE", ""))
+    }),
+
+  tar_target(
+    name = model_output,
+    command = make_trait_output(trait_mean)
+  ),
+
+
+  # FIGURE 2a: trait change along gradients
+  tar_target(
+    name = trait_plot,
+    command = {
+      make_trait_figure(trait_mean)
+    }),
+
+
   # make trait ordination
   tar_target(
     name = trait_pca_B,
@@ -44,10 +77,32 @@ analysis_plan <- list(
     command = make_trait_pca(trait_mean %>% filter(Gradient == "C"))
   ),
 
-  ## Intra vs. Inter
+  # FIGURE 2B: trait ordination
+  tar_target(
+    name = trait_ordination_plot,
+    command = make_trait_pca_plot(trait_pca_B, trait_pca_C)
+  ),
+
+  tar_target(
+    name = trait_ord_expl_var,
+    command = {
+
+      bind_cols(
+        bird = vegan::eigenvals(trait_pca_B[[3]])/sum(vegan::eigenvals(trait_pca_B[[3]])) * 100,
+        reference = vegan::eigenvals(trait_pca_C[[3]])/sum(vegan::eigenvals(trait_pca_C[[3]])) * 100)
+
+    }),
+
+  # INDIVIDUAL TRAITS
+
+  tar_target(
+    name = ind_species_figure,
+    command = make_ind_sp_plot(traits_raw, bryo_traits_raw)),
+
+  # ## Intra vs. Inter
   tar_target(
     name = variation_split_exp,
-    command = Intra_vs_Inter(trait_mean)
+    command = Intra_vs_Inter(traits_raw, trait_mean)
   ),
 
   tar_target(
@@ -55,33 +110,11 @@ analysis_plan <- list(
     command = Intra_vs_Inter_var_split(variation_split_exp)
   )
 
+  #make_intra_vs_inter_figure(var_split_exp, var_split)
 
-  # some preliminary rough analysis
-  # trait_mean %>%
-  #   group_by(trait_trans) %>%
-  #   nest(data = -c(trait_trans)) %>%
-  #   mutate(mod = map(data, ~lm(mean ~ Gradient * Site, data = .x)),
-  #          result = map(mod, tidy)) %>%
-  #   unnest(result) %>%
-  #   filter(p.value < 0.05) %>%
-  #   select(trait_trans, term:p.value) %>% View()
-  #
-  #
-  # diversity_grad %>%
-  #   group_by(DiversityIndex) %>%
-  #   nest(data = -c(DiversityIndex)) %>%
-  #   mutate(mod = map(data, ~lm(Value ~ Gradient * Site, data = .x)),
-  #          result = map(mod, tidy)) %>%
-  #   unnest(result) %>%
-  #   filter(p.value < 0.05)
 
 )
 
-
-# Check model fit for diversity Indices
-# library(performance)
-# dd <- diversity_grad %>%
-#   filter(DiversityIndex == "sumAbundance")
-# fit <- lm(Value ~ Gradient * Elevation_m, data = dd)
-# check_model(fit)
+#variation_split_exp <- Intra_vs_Inter(traits_raw, trait_mean)
+#variation_split <- Intra_vs_Inter_var_split(variation_split_exp)
 
