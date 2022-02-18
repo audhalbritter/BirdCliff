97bcd201b16669fad9cdf9c34e65194197ae3f3b
audhalbritter <me@audhalbritter.com>
2022-02-18 16:54:53 +0100

    ITV trial

--- _targets.R
+++ _targets.R
@@ -2,20 +2,27 @@ library("targets")
 library("tarchetypes")
 #remotes::install_github("Between-the-Fjords/dataDownloader")
 
-tar_option_set(packages = c("dataDownloader", "tidyverse", "readxl", "traitstrap", "vegan", "ggvegan", "kableExtra", "viridis", "patchwork"))
-
+tar_option_set(packages = c("dataDownloader", "tidyverse", "readxl", "traitstrap", "vegan", "ggvegan", "kableExtra", "viridis", "patchwork", "broom", "ape", "nlme", "Hmisc", "lme4", "broom.mixed", "ape", "nlme", "MuMIn"))
+#"performance",
 
 # source target plans - can also construct plans directly in this file.
 source("R/download_plan.R")
 source("R/transformation_plan.R")
 source("R/analysis_plan.R")
-source("R/figures_plan.R")
+source("R/si_figures_plan.R")
 source("R/manuscript_plan.R")
 
 # source functions
 source("R/functions/bootstrapping.R")
 source("R/functions/fancy_traits.R")
 source("R/functions/ordination.R")
+source("R/functions/intra_vs_inter.R")
+source("R/functions/inter_intra_anova.R")
+source("R/functions/correlation_plot.R")
+source("R/functions/trait-analysis.R")
+source("R/functions/trait_output.R")
+source("R/functions/ITV.R")
+source("R/functions/vascular_spcies.R")
 
 
 #Combine target plans
