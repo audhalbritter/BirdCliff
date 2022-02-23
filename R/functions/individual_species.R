18d9506c5a441b65348257a23c0d6ba21d7ac4a1
audhalbritter <me@audhalbritter.com>
2022-02-23 14:20:05 +0100

    reanalyse sp leve data with new elevation

--- R/functions/individual_species.R
+++ R/functions/individual_species.R
@@ -20,10 +20,10 @@ combine_traits <- function(traits_raw, bryo_traits_raw){
                             filter(Taxon %in% c("luzula confusa", "salix polaris"),
                                    trait_trans %in% c("Plant_Height_cm_log", "Leaf_Area_cm2_log", "LDMC", "N_percent", "dN15_permil")),
                           bryo_traits_raw %>%
-                            filter(Taxon %in% c("aulacomnium turgidum", "hylocomium splendens", "sanionia uncinata"),
+                            filter(Taxon %in% c("aulacomnium turgidum", "hylocomium splendens", "sanionia sp"),
                                    trait_trans %in% c("Shoot_Length_cm_log", "Shoot_ratio", "SSL_cm_g", "WHC_g_g", "P_percent"))) %>%
     mutate(trait_trans = factor(trait_trans, levels = c("Plant_Height_cm_log", "Leaf_Area_cm2_log", "LDMC", "N_percent", "dN15_permil", "Shoot_Length_cm_log", "Shoot_ratio", "SSL_cm_g", "WHC_g_g", "P_percent")),
-           Taxon = factor(Taxon, levels = c("luzula confusa", "salix polaris", "aulacomnium turgidum", "hylocomium splendens", "sanionia uncinata"))) %>%
+           Taxon = factor(Taxon, levels = c("luzula confusa", "salix polaris", "aulacomnium turgidum", "hylocomium splendens", "sanionia sp"))) %>%
     mutate(GS = paste0(Gradient, Site))
 
   return(ind_traits)
@@ -35,7 +35,7 @@ make_ind_sp_plot <- function(ind_traits){
   best_model <- ind_traits %>%
     filter(Functional_group == "vascular") %>%
     distinct(Taxon, trait_trans) %>%
-    mutate(best = c(NA_character_, "G+E", "G+E", "G", "G", "G+E", "G+E", "GxE", "G", "G"),
+    mutate(best = c("NA", "Null", "G+E", "Null", "G", "G+E", "G+E", "GxE", "G", "Gx"),
            x.pos = c(rep(10, 10)),
            y.pos = c(2.8, 0.8, 0.5, 3.8, 12, 2.8, 0.8, 0.5, 3.8, 12))
 
@@ -75,6 +75,28 @@ make_ind_sp_plot <- function(ind_traits){
 
 
 
+make_bryo_trait_model <- function(){
+  bryo_trait_output <- fancy_trait_name_dictionary(ind_traits) %>%
+    filter(Functional_group == "bryophyte") %>%
+    group_by(Taxon, trait_fancy) %>%
+    nest(data = -c(Taxon, trait_fancy)) %>%
+    mutate(estimate = map(data, ~{
+      mod <- lm(value_trans ~ Gradient*Elevation_m, data =  .x)
+      estimates = tidy(mod)
+    })) %>%
+    unnest(estimate) %>%
+    mutate(term = recode(term, "(Intercept)" = "Intercept", "Elevation_m" = "E", "GradientC" = "G", "GradientC:Elevation_m" = "GxE")) %>%
+    mutate(estimate = round(estimate, digits = 2),
+           std.error = round(std.error, digits = 2),
+           statistic = round(statistic, digits = 2),
+           p.value = round(p.value, digits = 3)) %>%
+    select(Taxon, Trait = trait_fancy, Term = term, "Std. error" = std.error, "t value" = statistic, "p value" = p.value) %>%
+    arrange(Taxon, Trait)
+
+  return(bryo_trait_output)
+
+}
+
 
 # Model selection (DOES NOT RUN!!!)
 run_ind_model_sel <- function(ind_traits){
@@ -92,10 +114,16 @@ run_ind_model_sel <- function(ind_traits){
 
 }
 
-# manual model selection
+
+
+
+# ind_traits %>% distinct(Taxon)
+# ind_traits %>% distinct(trait_trans)
+#
+# # manual model selection
 # dd <- ind_traits %>%
-#   filter(trait_trans == "WHC_g_g",
-#          Taxon == "hylocomium splendens")
+#   filter(trait_trans == "dN15_permil",
+#          Taxon == "poa arctica")
 # fit <- lmer(value_trans ~ Gradient * Elevation_m + (1|GS), na.action = "na.fail", REML = FALSE, data = dd)
 # model.set = dredge(fit, rank = "AICc", extra = "R^2")
 # model.set
@@ -116,10 +144,34 @@ run_ind_model_sel <- function(ind_traits){
 # Run models for single species
 run_ind_models <- function(ind_traits){
 
+  ### NULL MODEL
+  dat <- ind_traits %>%
+    filter(Taxon == "salix polaris" &  trait_trans %in% c("Leaf_Area_cm2_log", "N_percent"))
+
+  estimate_n <- dat %>%
+    group_by(Taxon, trait_trans) %>%
+    nest(data = -c(Taxon, trait_trans)) %>%
+    mutate(estimate = map(data, ~{
+      mod <- lmer(value_trans ~ 1 + (1|GS), data =  .x)
+      estimates = broom.mixed::tidy(mod)
+    })) %>%
+    unnest(estimate)
+
+  r_square_n <- dat %>%
+    group_by(Taxon, trait_trans) %>%
+    nest(data = -c(Taxon, trait_trans)) %>%
+    mutate(r = map(data, ~{
+      mod <- lmer(value_trans ~ 1 + (1|GS), data =  .x)
+      r = as.numeric(r.squaredGLMM(mod))
+    })) %>%
+    unnest_wider(col = r) %>%
+    select(trait_trans, "Rm" = "...1", "Rc" = "...2")
+
+
   ### GRADIENT MODEL
   dat <- ind_traits %>%
-    filter(Taxon %in% c("salix polaris", "luzula confusa"),
-           trait_trans %in% c("N_percent", "dN15_permil"))
+    filter(Taxon == "salix polaris" &  trait_trans == "dN15_permil" |
+           Taxon == "luzula confusa" & trait_trans == "N_percent")
 
   estimate_g <- dat %>%
     group_by(Taxon, trait_trans) %>%
@@ -143,8 +195,8 @@ run_ind_models <- function(ind_traits){
 
   ### GRADIENT + ELEVATION MODEL
   dat <- ind_traits %>%
-    filter(Taxon %in% c("salix polaris") & trait_trans %in% c("Leaf_Area_cm2_log", "LDMC") |
-             Taxon %in% c("luzula confusa")  & trait_trans %in% c("Plant_Height_cm_log", "Leaf_Area_cm2_log"))
+    filter(Taxon == "salix polaris" & trait_trans == "LDMC" |
+             Taxon == "luzula confusa"  & trait_trans %in% c("Plant_Height_cm_log", "Leaf_Area_cm2_log"))
 
   estimate_ge <- dat %>%
     group_by(Taxon, trait_trans) %>%
@@ -168,7 +220,7 @@ run_ind_models <- function(ind_traits){
 
   ### GRADIENT * ELEVATION MODEL
   dat <- ind_traits %>%
-    filter(Taxon %in% c("luzula confusa")  & trait_trans %in% c("LDMC"))
+    filter(Taxon %in% c("luzula confusa")  & trait_trans %in% c("LDMC", "dN15_permil"))
 
   estimate_gxe <- dat %>%
     group_by(Taxon, trait_trans) %>%
@@ -191,6 +243,7 @@ run_ind_models <- function(ind_traits){
 
 
   estimate <- bind_rows(
+    Null = estimate_n,
     G = estimate_g,
     GE = estimate_ge,
     GxE = estimate_gxe,
@@ -198,6 +251,7 @@ run_ind_models <- function(ind_traits){
   )
 
   r <- bind_rows(
+    Null = r_square_n,
     G = r_square_g,
     GE = r_square_ge,
     GxE = r_square_gxe,
