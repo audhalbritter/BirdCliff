30ddb3bcf7829f90e63182f488277a6e44b8e384
audhalbritter <me@audhalbritter.com>
2022-03-21 11:49:07 +0100

    make climate figure using correct model

--- R/si_figures_plan.R
+++ R/si_figures_plan.R
@@ -1,4 +1,4 @@
-figures_plan <- list(
+si_figures_plan <- list(
 
   # climate data
   tar_target(
@@ -40,24 +40,81 @@ figures_plan <- list(
     name = climate_plot,
     command = {
 
-      text = tibble(label = c("GxE", "G"),
-               Variable = c("soil moisture in %", "soil temperature in °C"),
-               Elevation_m = 125,
-               Value = c(50, 10),
-               Gradient = NA)
-      climate_data %>%
-        mutate(Variable = recode(Variable, "SoilMoisture" = "soil moisture in %", "SoilTemperature" = "soil temperature in °C")) %>%
-        left_join(coordinates, by = c("Gradient", "Site", "PlotID")) %>%
+      # climate data
+      dat <- climate_data %>%
+        mutate(Variable = recode(Variable, "SoilMoisture" = "soil moisture in %", "SoilTemperature" = "soil temperature in °C"),
+               GS = paste0(Gradient, Site)) %>%
+        left_join(coordinates, by = c("Gradient", "Site", "PlotID"))
+
+      # soil moisture
+      dd <- dat %>%
+        filter(Variable == "soil moisture in %")
+      fit <- lmer(Value ~  Gradient * Elevation_m + (1|GS), data = dd)
+
+      newdat <- dd %>%
+        distinct(Elevation_m, Gradient) %>%
+        mutate(Value = 0)
+      newdat$Value <-  predict(fit, newdat, re.form = NA)
+
+      mm <- model.matrix(terms(fit), newdat)
+
+      soil_m <- newdat %>%
+        mutate(pvar1 = diag(mm %*% tcrossprod(vcov(fit), mm)),
+               tvar1 = pvar1 + VarCorr(fit)$GS[1],  ## must be adapted for more complex models
+               cmult = 1.96) %>%
+        mutate(plo = Value - cmult*sqrt(pvar1),
+               phi = Value + cmult*sqrt(pvar1),
+               tlo = Value - cmult*sqrt(tvar1),
+               thi = Value + cmult*sqrt(tvar1))
+
+      g0 <- dat |>
+        filter(Variable == "soil moisture in %") |>
         ggplot(aes(x = Elevation_m, y = Value, colour = Gradient, fill = Gradient)) +
         geom_point(alpha = 0.5) +
-        geom_smooth(method = "lm") +
         scale_colour_manual(values = c("green4", "grey"), labels = c("Bird cliff", "Reference")) +
-        scale_fill_manual(values = c("green4", "grey"), labels = c("Bird cliff", "Reference")) +
-        geom_text(data = text, aes(label = label), colour = "black") +
         labs(x = "Elevation in m a.s.l.", y = "") +
-        facet_wrap(~ Variable, scales = "free_y") +
         theme_minimal() +
         theme(legend.position = "bottom")
+
+        soil_m_plot <- g0 +
+        geom_line(data = soil_m) +
+        geom_ribbon(data = soil_m, aes(ymin = plo, ymax = phi, fill = Gradient), alpha = 0.3, linetype = 0) +
+        scale_fill_manual(values = c("green4", "grey"), labels = c("Bird cliff", "Reference")) +
+        annotate("text", x = Inf, y = Inf, label = "GxE", size = 3, hjust = 1, vjust = 1) +
+        labs(y = "Soil moisture in %")
+
+
+
+      # soil temperature
+      dd <- dat %>%
+        filter(Variable == "soil temperature in °C")
+      fit <- lmer(Value ~  Gradient + (1|GS), data = dd)
+
+      newdat <- dd %>%
+        distinct(Elevation_m, Gradient) %>%
+        mutate(Value = 0)
+      newdat$Value <-  predict(fit, newdat, re.form = NA)
+
+      mm <- model.matrix(terms(fit), newdat)
+
+      soil_t <- newdat %>%
+        mutate(pvar1 = diag(mm %*% tcrossprod(vcov(fit), mm)),
+               tvar1 = pvar1 + VarCorr(fit)$GS[1],  ## must be adapted for more complex models
+               cmult = 1.96) %>%
+        mutate(plo = Value - cmult*sqrt(pvar1),
+               phi = Value + cmult*sqrt(pvar1),
+               tlo = Value - cmult*sqrt(tvar1),
+               thi = Value + cmult*sqrt(tvar1))
+
+      soil_t_plot <- g0 %+% subset(dat, Variable == "soil temperature in °C") +
+        geom_line(data = soil_t) +
+        geom_ribbon(data = soil_t, aes(ymin = plo, ymax = phi, fill = Gradient), alpha = 0.3, linetype = 0) +
+        scale_fill_manual(values = c("green4", "grey"), labels = c("Bird cliff", "Reference")) +
+        annotate("text", x = Inf, y = Inf, label = "G", size = 3, hjust = 1, vjust = 1) +
+        labs(y = "Soil temperature in °C")
+
+      climate_plot <- soil_m_plot + soil_t_plot + plot_layout(guides = 'collect') & theme(legend.position = 'bottom')
+
     }),
 
 
