ab43057382865584ca7efafc6080ee7f52b7a3ec
audhalbritter <me@audhalbritter.com>
2022-12-09 09:24:50 +0100

    move nmds code to new script and make nutrient cylcing arrow light grey

--- R/functions/ordination.R
+++ R/functions/ordination.R
@@ -1,145 +1,5 @@
 # ORDINATIONS
 
-# COMMUNITY
-check_dimensions_NMDS <- function(comm_raw){
-
-  set.seed(32)
-
-  comm_fat <- comm_raw %>%
-    group_by(Site) %>%
-    mutate(Mean_elevation = mean(Elevation_m)) %>%
-    ungroup() %>%
-    select(Gradient, Site, GS, Mean_elevation, PlotID, Taxon) %>%
-    mutate(presence = 1) %>%
-    pivot_wider(names_from = Taxon,
-                values_from = presence,
-                values_fill = 0)
-
-  comm_fat_spp <- comm_fat %>% select(-(Gradient:PlotID))
-
-  NMDS_1 <-  metaMDS(comm_fat_spp, noshare = TRUE, try = 30, k = 1)
-  NMDS_2 <-  metaMDS(comm_fat_spp, noshare = TRUE, try = 30, k = 2)
-  NMDS_3 <-  metaMDS(comm_fat_spp, noshare = TRUE, try = 30, k = 3)
-  NMDS_4 <-  metaMDS(comm_fat_spp, noshare = TRUE, try = 30, k = 4)
-  NMDS_5 <-  metaMDS(comm_fat_spp, noshare = TRUE, try = 30, k = 5)
-  NMDS_6 <-  metaMDS(comm_fat_spp, noshare = TRUE, try = 30, k = 6)
-
-  stress_plot <- tibble(
-    stress = c(NMDS_1$stress, NMDS_2$stress, NMDS_3$stress, NMDS_4$stress, NMDS_5$stress, NMDS_6$stress),
-    dimensions = c(1:6)) %>%
-    ggplot(aes(x = dimensions, y = stress)) +
-    geom_point()
-
-  return(stress_plot)
-}
-
-make_ordination <- function(comm_raw){
-
-  set.seed(32)
-
-  comm_fat <- comm_raw %>%
-    group_by(Site) %>%
-    mutate(Mean_elevation = mean(Elevation_m)) %>%
-    ungroup() %>%
-    select(Gradient, Site, GS, Mean_elevation, PlotID, Taxon) %>%
-    mutate(presence = 1) %>%
-    pivot_wider(names_from = Taxon,
-                values_from = presence,
-                values_fill = 0)
-
-  comm_fat_spp <- comm_fat %>% select(-(Gradient:PlotID))
-
-  NMDS <-  metaMDS(comm_fat_spp, noshare = TRUE, try = 30, k = 2)
-
-  stress <- NMDS$stress
-
-  #stressplot(NMDS)
-
-  # fortify
-  fNMDS <- fortify(NMDS) %>%
-    filter(Score == "sites") %>%
-    bind_cols(comm_fat %>% select(Gradient:PlotID))
-
-  return(list(NMDS, fNMDS, stress))
-}
-
-
-### test model using adonis
-test_ordination <- function(comm_raw){
-
-
-  comm_fat <- comm_raw %>%
-    group_by(Site) %>%
-    mutate(Mean_elevation = mean(Elevation_m)) %>%
-    ungroup() %>%
-    select(Gradient, Site, GS, Mean_elevation, PlotID, Taxon) %>%
-    mutate(presence = 1) %>%
-    pivot_wider(names_from = Taxon,
-                values_from = presence,
-                values_fill = 0)
-
-  comm_fat_spp <- comm_fat %>% select(-(Gradient:PlotID))
-  meta <- comm_fat %>% select(Gradient:PlotID)
-
-  adon.results <- adonis(comm_fat_spp ~ Gradient*Mean_elevation, data = meta, method = "bray", perm = 999)
-
-  return(adon.results)
-
-  }
-
-
-
-# figure
-make_ordination_plot <- function(comm_raw, NMDS, fNMDS){
-
-  # env <- comm_raw %>%
-  #   group_by(Site) %>%
-  #   mutate(Mean_elevation = mean(Elevation_m)) %>%
-  #   ungroup() %>%
-  #   select(Gradient, Site, GS, Mean_elevation, PlotID, Taxon) %>%
-  #   mutate(presence = 1) %>%
-  #   pivot_wider(names_from = Taxon,
-  #               values_from = presence,
-  #               values_fill = 0) %>%
-  #   select(Gradient:PlotID)
-  #
-  # # extract NMDS scores
-  # sites <- as.data.frame(scores(NMDS, display = "sites"))
-  # sites <- cbind(sites, GS = env$GS)
-  # sites$GS <- factor(sites$GS)
-  #
-  # ### NMDS 1 and 2
-  # #data for ellipse, in this case using the management factor
-  # df_ell_12 <- data.frame() #sets up a data frame before running the function.
-  # for(g in levels(sites$GS)){
-  #   df_ell_12 <- rbind(df_ell_12, cbind(as.data.frame(with(sites [sites$GS==g,],
-  #                                                    veganCovEllipse(cov.wt(cbind(NMDS1,NMDS2),wt=rep(1/length(NMDS1),length(NMDS1)))$cov,center=c(mean(NMDS1),mean(NMDS2))))) ,GS=g))
-  # }
-  #
-  # df_ell_12 <- df_ell_12 %>%
-  #   left_join(env %>% distinct(GS, Gradient, Site, Elevation_m), by = "GS")
-
-  NMDS_plot <- fNMDS %>%
-    ggplot(aes(x = NMDS1, y = NMDS2, group = GS, colour = Mean_elevation, shape = Gradient, linetype = Gradient)) +
-    geom_point(size = 2) +
-    stat_ellipse() +
-    coord_equal() +
-    scale_colour_viridis_c(name = "Elevation in m a.s.l.",
-                           end = 0.8,
-                           option = "inferno",
-                           direction = -1) +
-    scale_linetype_manual(name = "", values = c(1, 2), , labels = c("Nutrient input", "Reference")) +
-    scale_shape_manual(name = "", values = c(16, 2), , labels = c("Nutrient input", "Reference")) +
-    labs(x = "NMDS axis 1", y = "NMDS axis 2") +
-    theme_minimal()
-
-  # ordi_plot12 <- g12 +
-  #   geom_path(data = df_ell_12, aes(x = NMDS1, y = NMDS2, group = GS, colour = Elevation_m)) +
-  #   scale_linetype_manual(values = c(1, 2), , labels = c("Bird cliff", "Reference"))
-
-  return(NMDS_plot)
-}
-
 ## COMMUNITY (PCA)
 
 make_community_pca <- function(comm_raw){
@@ -425,7 +285,7 @@ make_trait_pca_plot <- function(trait_pca_B, trait_pca_C){
     labs(x = "PC 1", y = "PC 2", tag = "(b)") +
     scale_x_continuous(expand = c(.2, 0)) +
     scale_linetype_manual(name = "", values = c("solid", "dashed", "solid")) +
-    scale_colour_manual(name = "", values = c("black", "grey40", "grey40")) +
+    scale_colour_manual(name = "", values = c("black", "grey40", "grey70")) +
     theme_minimal() +
     theme(aspect.ratio = 1,
           plot.tag.position = c(0, 1),
@@ -469,7 +329,7 @@ make_trait_pca_plot <- function(trait_pca_B, trait_pca_C){
     labs(x = "PC 1", y = "PC 2", tag = "(d)") +
     scale_x_continuous(expand = c(.2, 0)) +
     scale_linetype_manual(name = "", values = c("solid", "dashed", "solid")) +
-    scale_colour_manual(name = "", values = c("black", "grey40", "grey40")) +
+    scale_colour_manual(name = "", values = c("black", "grey40", "grey70")) +
     theme_minimal() +
     theme(aspect.ratio = 1,
           plot.tag.position = c(0, 1),
