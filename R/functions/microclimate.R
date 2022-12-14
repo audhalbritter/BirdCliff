5a420eaaafba0d4086fd3ec8d12ba3ddd1c9745f
audhalbritter <me@audhalbritter.com>
2022-12-14 08:10:17 +0100

    generalize microclimate analysis with new functions and run some tests for the importance of soil temp and moisture

--- R/functions/microclimate.R
+++ R/functions/microclimate.R
@@ -1,42 +1,8 @@
 ### CLIMATE DATA
 
-# make prediction for lmer
-lmer_climate_prediction <- function(dat, fit){
+climate_lrt <- function(climate_data){
 
-  newdat <- dat %>%
-    select(Gradient, Elevation_m) %>%
-    mutate(Value = 0)
-  newdat$Value <- predict(fit, newdat, re.form = NA)
-
-  mm <- model.matrix(terms(fit), newdat)
-
-  prediction <- newdat %>%
-    mutate(pvar1 = diag(mm %*% tcrossprod(vcov(fit), mm)),
-           tvar1 = pvar1 + VarCorr(fit)$GS[1],  ## must be adapted for more complex models
-           cmult = 1.96) %>%
-    mutate(plo = Value - cmult*sqrt(pvar1),
-           phi = Value + cmult*sqrt(pvar1),
-           tlo = Value - cmult*sqrt(tvar1),
-           thi = Value + cmult*sqrt(tvar1)) |>
-    rename(fitted = Value, Gradient_fit = Gradient, Elevation_fit = Elevation_m)
-
-  return(prediction)
-}
-
-
-make_climate_analysis <- function(climate_data, coordinate){
-
-  # quadratic model is better
-  model_choice <- climate_data |>
-    group_by(Variable) |>
-    nest() |>
-    mutate(lin_mod = map(data, ~ safely(lmer)(Value ~  Gradient * Elevation_m + (1|GS), data = .x)$result),
-           poly_mod = map(data, ~ safely(lmer)(Value ~  Gradient * poly(Elevation_m, 2) + (1|GS), data = .x)$result),
-           mod_aic = map2(.x = lin_mod, .y = poly_mod, .f = AIC)) |>
-    unnest(col = mod_aic) |>
-    mutate(Model = c("linear", "quadratic"))
-
-  lrt <- climate_data|>
+  climate_data |>
     group_by(Variable) |>
     nest() |>
     mutate(LTR = map(data, ~{
@@ -50,39 +16,28 @@ make_climate_analysis <- function(climate_data, coordinate){
     })) |>
     unnest(LTR)
 
-  out <- climate_data |>
-    # run model by trait
-    group_by(Variable) |>
-    nest() |>
-    mutate(model = map(data, ~ safely(lmer)(Value ~  Gradient * poly(Elevation_m, 2) + (1|GS), data = .x)$result),
-           model_output = map(model, tidy),
-           r = map(model, r.squaredGLMM),
-           r = map(r, as.numeric),
-           prediction = map2(.x = data, .y = model, ~ safely(lmer_climate_prediction)(.x, .y)$result))
-
-  return(list(out, lrt, model_choice))
-
 }
 
 
 # climate figure
-make_climate_figure <- function(climate_analysis){
+make_climate_figure <- function(climate_model_output){
 
-  out <- climate_analysis |>
+  out <- climate_model_output |>
     # merge data and prediction
     mutate(output = map2(.x = data, .y = prediction, ~ bind_cols(.x, .y))) |>
-    select(-data, -prediction, -model, -model_output, -r) |>
+    select(-data, -prediction, -mod, -model_output, -r) |>
     unnest(output) |>
-    rename(Value = Value...7, fitted = Value...14, Gradient = Gradient...4, Elevation_m = Elevation_m...9) |>
-    select(-Elevation_m...13, -Gradient...12) |>
-    mutate(Variable = recode(Variable, "SoilMoisture" = "Soil moisture in %", "SoilTemperature" = "Soil temperature in °C"))
+    rename(Value = .response...7, fitted = .response...14, Gradient = Gradient...4, Elevation_m = .continous_predictor...9) |>
+    select(-.continous_predictor...13, -Gradient...12) |>
+    mutate(Variable = recode(Variable, "SoilMoisture" = "Soil moisture in %", "SoilTemperature" = "Soil temperature in °C")) |>
+    mutate(text = "NxE")
 
   ggplot(out, aes(x = Elevation_m, y = Value, colour = Gradient)) +
     geom_point(alpha = 0.5) +
     geom_line(aes(y = fitted, colour = Gradient)) +
     geom_ribbon(aes(ymin = plo, ymax = phi, fill = Gradient), alpha = 0.3, linetype = 0) +
-    scale_colour_manual(name = "", values = c("green4", "grey"), labels = c("Nutrient input", "Reference")) +
-    scale_fill_manual(name = "", values = c("green4", "grey"), labels = c("Nutrient input", "Reference")) +
+    scale_colour_manual(name = "", values = c("grey", "green4"), labels = c("Reference", "Nutrient input")) +
+    scale_fill_manual(name = "", values = c("grey", "green4"), labels = c("Reference", "Nutrient input")) +
     labs(x = "Elevation m a.s.l.", y = "") +
     # add label
     geom_text(aes(x = Inf, y = Inf, label = "NxE"),
@@ -93,35 +48,113 @@ make_climate_figure <- function(climate_analysis){
 
 }
 
-# dd <- climate_data |>
-#   filter(Variable == "SoilTemperature") |>
-#   mutate(GS = paste0(Gradient, Site))
-# fit <- lmer(Value ~ Gradient * poly(Elevation_m, 2) + (1|GS), dd)
-# check_model(fit)
-# dd <- trait_mean |>
-#   filter(trait_trans == "Thickness_mm_log")
-# fit <- lmer(mean ~ Gradient * poly(Elevation_m, 2) + SoilTemperature + (1|GS), dd)
-### SLA singular fit
 
-# test linear vs polynomial model
-lin_vs_poly_model <- function(trait_mean){
+# climate output: model output and r2
+make_climate_table <- function(climate_model_output){
 
-  trait_mean |>
-    group_by(trait_trans) |>
-    nest() |>
-    mutate(lin_mod = map(data, ~ safely(lmer)(mean ~  Gradient * SoilTemperature + (1|GS), data = .x)$result),
-           poly_mod = map(data, ~ safely(lmer)(mean ~  Gradient * poly(SoilTemperature, 2) + (1|GS), data = .x)$result),
-           mod_aic = map2(.x = lin_mod, .y = poly_mod, .f = AIC)) |>
-    unnest(col = mod_aic) |>
-    mutate(Model = c("linear", "quadratic"))
+  climate_model_output |>
+    select(-data, -mod, -singular, -aic, -prediction, -r) |>
+    unnest(model_output) %>%
+    filter(effect == "fixed") %>%
+    ungroup() |>
+    select(Variable, term, estimate:statistic) %>%
+    mutate(term = recode(term,
+                         "(Intercept)" = "Intercept",
+                         "GradientB" = "N",
+                         "poly(.continous_predictor, 2)1" = "E",
+                         "poly(.continous_predictor, 2)2" = "E2",
+                         "GradientB:poly(.continous_predictor, 2)1" = "NxE",
+                         "GradientB:poly(.continous_predictor, 2)2" = "NxE2")) %>%
+    # join R squared
+    left_join(climate_model_output |>
+                select(-data, -mod, -singular, -aic, -prediction, -model_output) |>
+                unnest_wider(col = r) |>
+                rename(Rm = ...1, Rc = ...2) |>
+                ungroup() %>%
+                select(Variable, Rm, Rc),
+              by = "Variable") %>%
+    mutate(estimate = round(estimate, digits = 2),
+           std.error = round(std.error, digits = 2),
+           statistic = round(statistic, digits = 2),
+           Rm = round(Rm, digits = 2),
+           Rc = round(Rc, digits = 2)) %>%
+    select(Variable, Term = term, Estimate = estimate, "Std error" = std.error, "t-value" = "statistic", "Marginal R2" = Rm, "Conditional R2" = Rc) %>%
+    arrange(Variable) %>%
+    write_csv(., file = "output/Climate_output.csv")
+
 }
 
+
+### CLIMATE AND TRAITS
+
+# run linear and quadratic model
+run_trait_climate_model <- function(dat, group, response, continous_predictor){
+
+  dat |>
+    rename(.response = {{response}},
+           .continous_predictor = {{continous_predictor}}) |>
+    group_by(across(all_of({{group}})))|>
+    nest() |>
+    mutate(
+      # linear model
+      mod_linear = map(data, ~ safely(lmer)(.response ~  Gradient * .continous_predictor + SoilTemperature + (1|GS), data = .x)$result),
+      singular_linear = map_lgl(mod_linear, isSingular),
+      aic_linear = map(.x = mod_linear, .f = ~ safely(AIC)(.x)$result),
+      aic_linear = as.numeric(aic_linear),
+
+      # quadratic
+      mod_quadratic = map(data, ~ safely(lmer)(.response ~  Gradient * poly(.continous_predictor, 2) + (1|GS), data = .x)$result),
+      singular_quadratic = map_lgl(mod_quadratic, isSingular),
+      aic_quadratic = map(.x = mod_quadratic, .f = ~ safely(AIC)(.x)$result),
+      aic_quadratic = as.numeric(aic_quadratic),
+
+      # quadratic plus SoilTemp
+      mod_quadraticPlus = map(data, ~ safely(lmer)(.response ~  Gradient * poly(.continous_predictor, 2) + SoilTemperature + (1|GS), data = .x)$result),
+      aic_quadraticPlus = map(.x = mod_quadraticPlus, .f = ~ safely(AIC)(.x)$result),
+      aic_quadraticPlus = as.numeric(aic_quadraticPlus))
+}
+
+# run linear and quadratic model
+# tar_target(
+#   name = trait_community_model,
+#   command = run_trait_climate_model(dat = trait_mean,
+#                             group = "trait_trans",
+#                             response = mean,
+#                             continous_predictor = Elevation_m) |>
+#     pivot_longer(cols = -c(trait_trans, data),
+#                  names_sep = "_",
+#                  names_to = c(".value", "names")) |>
+#     filter(singular == FALSE | is.na(singular)) |>
+#     filter(aic == min(aic))
+# ),
+
+
+### ALTERNATIVE APPROACH
+# soil temp/moisture replacing elevation
+# run_trait_model(dat = trait_mean,
+#                         group = "trait_trans",
+#                         response = mean,
+#                         continous_predictor = SoilTemperature) |>
+#   pivot_longer(cols = -c(trait_trans, data),
+#                names_sep = "_",
+#                names_to = c(".value", "names")) |>
+#   filter(singular == "FALSE") |>
+#   filter(aic == min(aic))
+#
+# run_trait_model(dat = trait_mean,
+#                 group = "trait_trans",
+#                 response = mean,
+#                 continous_predictor = SoilMoisture) |>
+#   pivot_longer(cols = -c(trait_trans, data),
+#                names_sep = "_",
+#                names_to = c(".value", "names")) |>
+#   filter(singular == "FALSE") |>
+#   filter(aic == min(aic))
 
 
 likelihood_ratio_test_ST <- function(trait_mean){
 
   trait_mean |>
-    #filter(trait_trans != "SLA_cm2_g") |>
     group_by(trait_trans) |>
     nest() |>
     mutate(LTR = map(data, ~{
@@ -134,9 +167,15 @@ likelihood_ratio_test_ST <- function(trait_mean){
       # ltr
       test = anova(ExN, EplusN, N, E, Null)
     })) |>
-    unnest(LTR)
+    unnest(LTR) |>
+    filter(`Pr(>Chisq)` <= 0.05)
 
 }
+# PH: NxST
+# N%: N+ST
+# CN: N+ST
+# dC13: NxST
+# dN15: N+ST
 
 likelihood_ratio_test_SM <- function(trait_mean){
 
