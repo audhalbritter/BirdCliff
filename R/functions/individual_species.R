8ae7c4733aab789b49855e6764d2f9e31d9e1da8
audhalbritter <me@audhalbritter.com>
2022-12-14 08:11:11 +0100

    make ind plant and bryophyte analysis more general

--- R/functions/individual_species.R
+++ R/functions/individual_species.R
@@ -58,6 +58,45 @@ likelihood_ratio_test_vasc <- function(ind_traits){
 }
 
 
+# vascular trait output: model output and r2
+make_vascular_table <- function(vascular_model_output){
+
+  vascular_model_output |>
+    select(-data, -names, -mod, -singular, -aic, -prediction, -r) |>
+    unnest(model_output) %>%
+    fancy_trait_name_dictionary(.) %>%
+    filter(effect == "fixed") %>%
+    ungroup() |>
+    select(Taxon, Trait = trait_fancy, text, term, estimate:statistic, class) %>%
+    mutate(term = recode(term,
+                         "(Intercept)" = "Intercept",
+                         "GradientB" = "N",
+                         ".continous_predictor" = "E",
+                         "GradientB:.continous_predictor" = "NxE" ,
+                         "poly(.continous_predictor, 2)1" = "E",
+                         "poly(.continous_predictor, 2)2" = "E2",
+                         "GradientB:poly(.continous_predictor, 2)1" = "NxE",
+                         "GradientB:poly(.continous_predictor, 2)2" = "NxE2")) %>%
+    # join R squared
+    left_join(vascular_model_output |>
+                select(-data, -mod, -singular, -aic, -prediction, -model_output) |>
+                unnest_wider(col = r) |>
+                rename(Rm = ...1, Rc = ...2) |>
+                ungroup() %>%
+                fancy_trait_name_dictionary(.) %>%
+                select(Taxon, trait_fancy, Rm, Rc),
+              by = c("Taxon", "Trait" = "trait_fancy")) %>%
+    mutate(estimate = round(estimate, digits = 2),
+           std.error = round(std.error, digits = 2),
+           statistic = round(statistic, digits = 2),
+           Rm = round(Rm, digits = 2),
+           Rc = round(Rc, digits = 2)) %>%
+    select(Class = class, Taxon, Trait, Term = term, Estimate = estimate, "Std error" = std.error, "t-value" = "statistic", "Marginal R2" = Rm, "Conditional R2" = Rc) %>%
+    arrange(Taxon, Class, Trait) %>%
+    write_csv(., file = "output/Vascular_trait_output.csv")
+
+}
+
 # trait figure
 make_vascular_figure <- function(vascular_model_output){
 
@@ -95,134 +134,87 @@ make_vascular_figure <- function(vascular_model_output){
 }
 
 
+
+
 ### BRYOPHYTES
 
-lin_poly_bryophytes <- function(ind_traits){
+# run linear and quadratic model
+run_bryophyte_model <- function(dat, group, response, continous_predictor){
 
-  ind_traits |>
-    filter(Functional_group == "bryophyte") |>
-    group_by(Taxon, trait_trans) |>
+  dat |>
+    rename(.response = {{response}},
+           .continous_predictor = {{continous_predictor}}) |>
+    group_by(across(all_of({{group}})))|>
     nest() |>
-    mutate(lin_mod = map(data, ~ safely(lm)(value_trans ~  Gradient * Elevation_m, data = .x)$result),
-           poly_mod = map(data, ~ safely(lm)(value_trans ~  Gradient * poly(Elevation_m, 2), data = .x)$result),
-           mod_aic = map2(.x = lin_mod, .y = poly_mod, .f = AIC)) |>
-    unnest(col = mod_aic) |>
-    mutate(Model = c("linear", "quadratic")) |>
-    filter(AIC == min(AIC))
-
+    mutate(mod_linear = map(data, ~ safely(lm)(.response ~  Gradient * .continous_predictor, data = .x)$result),
+           mod_quadratic = map(data, ~ safely(lm)(.response ~  Gradient * poly(.continous_predictor, 2), data = .x)$result),
+           aic_linear = map(.x = mod_linear, .f = ~ safely(AIC)(.x)$result),
+           aic_linear = as.numeric(aic_linear),
+           aic_quadratic = map(.x = mod_quadratic, .f = ~ safely(AIC)(.x)$result),
+           aic_quadratic = as.numeric(aic_quadratic))
 }
 
 
-# bryophyte models
-make_bryo_trait_model <- function(ind_traits){
 
-  bryo_trait_output <- fancy_trait_name_dictionary(ind_traits) %>%
-    filter(Functional_group == "bryophyte") %>%
-    group_by(Taxon, trait_fancy) %>%
-    nest(data = -c(Taxon, trait_fancy)) %>%
-    mutate(estimate = map(data, ~{
-      mod <- lm(value_trans ~ Gradient*Elevation_m, data =  .x)
-      estimates = tidy(mod)
-    })) %>%
-    unnest(estimate) %>%
-    mutate(term = recode(term, "(Intercept)" = "Intercept", "Elevation_m" = "E", "GradientC" = "G", "GradientC:Elevation_m" = "GxE")) %>%
+# Bryophyte table
+make_bryo_table <- function(bryophyte_model_output){
+
+  fancy_trait_name_dictionary(bryophyte_model_output) |>
+    unnest(model_output) %>%
+    ungroup() |>
+    select(-data, -names, -mod, -aic, -prediction) |>
+    mutate(term = recode(term,
+                         "(Intercept)" = "Intercept",
+                         ".continous_predictor" = "E",
+                         "GradientB" = "G",
+                         "GradientB:.continous_predictor" = "GxE",
+                         "poly(.continous_predictor, 2)1" = "E",
+                         "poly(.continous_predictor, 2)2" = "E^2^",
+                         "GradientB:poly(.continous_predictor, 2)1" = "GxE",
+                         "GradientB:poly(.continous_predictor, 2)2" = "GxE^2^")
+
+           ) %>%
     mutate(estimate = round(estimate, digits = 2),
            std.error = round(std.error, digits = 2),
            statistic = round(statistic, digits = 2),
            p.value = round(p.value, digits = 3)) %>%
-    select(Taxon, Trait = trait_fancy, Term = term, Estimate = estimate, "Std. error" = std.error, "t value" = statistic, "p value" = p.value) %>%
-    arrange(Taxon, Trait)
-
-  write_csv(bryo_trait_output, file = "output/bryo_trait_output.csv")
-
-  return(bryo_trait_output)
+    select(Class = class, Taxon, Trait = trait_fancy, Term = term, Estimate = estimate, "Std. error" = std.error, "t value" = statistic, "p value" = p.value) %>%
+    arrange(Taxon, Trait) %>%
+    write_csv(., file = "output/bryo_trait_output.csv")
 
 }
 
+# make bryophyte figure
+make_bryo_figure <- function(bryophyte_model_output){
+
+  out <- fancy_trait_name_dictionary(bryophyte_model_output) %>%
+    select(-mod, -aic, -model_output) |>
+    unnest(prediction) |>
+    mutate(text = case_when(trait_trans == "Shoot_ratio" ~ "",
+                            Taxon == "aulacomnium turgidum" & trait_trans == "Shoot_Length_cm_log" ~ "",
+                            Taxon == "sanionia sp" & trait_trans == "P_percent" ~ "E",
+                            Taxon == "hylocomium splendens" & trait_trans == "Shoot_Length_cm_log" ~ "E",
+                            Taxon == "hylocomium splendens" & trait_trans == "WHC_g_g" ~ "N",
+                            TRUE ~ "NxE")) |>
+    mutate(Taxon = recode(Taxon, "sanionia sp" = "Sanionia sp", "hylocomium splendens" = "Hylocomium splendens", "aulacomnium turgidum" = "Aulacomnium turgidum"))
 
 
-make_bryo_figure <- function(ind_traits, bryo_trait_output){
-
-  ### BRYO FIGURE
-
-  term = tibble(term = c("E", " ", "NxE", "NxE", "NxE", " ", " ", "N", " ", "NxE", " ", " ", " ", " ", " "))
-
-  result <- bryo_trait_output |>
-    distinct(Taxon, Trait) |>
-   bind_cols(term = term) |>
-    mutate(pvalue = if_else(term == " ", "non-sign", "sign"))
-
-  b_dat <- fancy_trait_name_dictionary(ind_traits) %>%
-    filter(Functional_group == "bryophyte") |>
-    left_join(result, by = c("Taxon", "trait_fancy" = "Trait")) |>
-    mutate(pvalue = factor(pvalue, levels = c("sign", "non-sign")),
-           Taxon = recode(Taxon, "sanionia sp" = "Sanionia sp", "hylocomium splendens" = "Hylocomium splendens", "aulacomnium turgidum" = "Aulacomnium turgidum"))
-
-  bryo_plot <- b_dat |>
-    ggplot(aes(x = Elevation_m, y = value_trans, colour = Gradient, linetype = pvalue, shape = class)) +
+ ggplot(out, aes(x = .continous_predictor, y = .response, colour = Gradient)) +
     geom_point(alpha = 0.5) +
-    geom_smooth(mapping = aes(fill = Gradient), b_dat |> filter(pvalue == "sign"), method = "lm", se = TRUE) +
-    scale_colour_manual(name = "", values = c("green4", "grey"), labels = c("Nutrient input", "Reference")) +
-    scale_fill_manual(name = "", values = c("green4", "grey"), labels = c("Nutrient input", "Reference")) +
-    scale_shape_manual(values = c(16, 17)) +
+    geom_line(aes(y = .fitted, colour = Gradient)) +
+    geom_ribbon(aes(ymin = .lower, ymax = .upper, fill = Gradient), alpha = 0.3, linetype = 0) +
+    scale_colour_manual(name = "", values = c("grey", "green4"), labels = c("Reference", "Nutrient input")) +
+    scale_fill_manual(name = "", values = c("grey", "green4"), labels = c("Reference", "Nutrient input")) +
     labs(x = "Elevation in m a.s.l.", y = "Trait value") +
-    geom_text(ata = b_dat |> distinct(Taxon, trait_fancy, term) , aes(x = Inf, y = Inf, label = term), size = 3, colour = "black", hjust = 1, vjust = 1) +
+    geom_text(data = out |>
+                distinct(Taxon, trait_fancy, text),
+              aes(x = Inf, y = Inf, label = text),
+              size = 3, colour = "black", hjust = 1, vjust = 1) +
     facet_grid(trait_fancy ~ Taxon, scales = "free_y") +
     theme_minimal() +
     theme(legend.position = "top",
           plot.title = element_text(size = 10),
           strip.text.x = element_text(face = "italic"),
-          aspect.ratio = 0.7) +
-    guides(linetype = "none")
+          aspect.ratio = 0.7)
 
-  # bryo_plot_leaf_eco <- b_dat |>
-  #   filter(class == "Leaf economic") |>
-  #   ggplot(aes(x = Elevation_m, y = value_trans, colour = Gradient, linetype = pvalue)) +
-  #   geom_point(alpha = 0.5) +
-  #   geom_smooth(mapping = aes(fill = Gradient), b_dat |> filter(class == "Leaf economic", pvalue == "sign"), method = "lm", se = TRUE) +
-  #   scale_colour_manual(name = "", values = c("green4", "grey"), labels = c("Nutrient input", "Reference")) +
-  #   scale_fill_manual(name = "", values = c("green4", "grey"), labels = c("Nutrient input", "Reference")) +
-  #   labs(x = "Elevation in m a.s.l.", y = "Trait value") +
-  #   geom_text(ata = b_dat |> distinct(Taxon, trait_fancy, term) , aes(x = Inf, y = Inf, label = term), size = 3, colour = "black", hjust = 1, vjust = 1) +
-  #   facet_grid(trait_fancy ~ Taxon, scales = "free_y") +
-  #   theme_minimal() +
-  #   theme(legend.position = "top",
-  #         plot.title = element_text(size = 10),
-  #         strip.text.x = element_text(face = "italic"),
-  #         aspect.ratio = 0.7) +
-  #   guides(linetype = "none")
-  #
-  # bryo_plot_size / bryo_plot_leaf_eco +
-  #   plot_layout(guides = 'collect',
-  #               widths = c(1, 5),
-  #               heights = c(1, 4)) &
-  #   theme(legend.position = 'top')
-
-  return(bryo_plot)
 }
-
-
-# ind_traits %>% distinct(Taxon)
-# ind_traits %>% distinct(trait_trans)
-#
-# # manual model selection
-# dd <- ind_traits %>%
-#   filter(trait_trans == "dN15_permil",
-#          Taxon == "poa arctica")
-# fit <- lmer(value_trans ~ Gradient * Elevation_m + (1|GS), na.action = "na.fail", REML = FALSE, data = dd)
-# model.set = dredge(fit, rank = "AICc", extra = "R^2")
-# model.set
-
-# Results from model selection
-# sal.pol A: G+E, LDMC: G+E, N: G, N15: G
-# #cer.arc LDMC: Null (removed!)
-# luz.conf H: G+E, A: G+E, LDMC: GxE, N: G, N15: G
-#
-# Bryos: too little data and most are Null model!!!
-# san.unc WHC: G
-# aul.tur P: Null
-# hyl spl SL: Null, SSL: Null
-
-
-
-
