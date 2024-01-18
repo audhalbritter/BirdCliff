4a25ac533f1fff51c507f278939409a5971ac816
audhalbritter <me@audhalbritter.com>
2024-01-18 17:28:43 +0100

    fix fortify

--- R/transformation_plan.R
+++ R/transformation_plan.R
@@ -134,10 +134,11 @@ transformation_plan <- list(
   # trait coverage
   tar_target(
     name = trait_coverage,
-    command = fortify(trait_impute) |>
+    command = fortify_filled_trait(trait_impute) |>
       ungroup() |>
       #filter(Trait == "CN_ratio") |>
-      complete(.id, level, trait_trans, fill = list(s = 0)) |>
+      complete(.id, level, trait_trans, fill = list(s = 0)) |> # .id does not exist anymore...
+      complete(level, trait_trans, fill = list(s = 0)) |>
       filter(level == "PlotID") |>
       group_by(Gradient, trait_trans) |>
       # prob = 0.25 gives 75% of the plots
