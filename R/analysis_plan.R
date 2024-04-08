7acc6dbf014eb2a5cfb7525feac8e9ca3ec22063
audhalbritter <me@audhalbritter.com>
2024-04-08 14:28:48 +0200

    adapt soil dN15 analysis remove unused code get correlation between soil and leaf dN15

--- R/analysis_plan.R
+++ R/analysis_plan.R
@@ -20,7 +20,7 @@ analysis_plan <- list(
 
   tar_target(
     name = adonis_comm_output,
-    command = tidy(comm_pca[[4]]$aov.tab)
+    command = tidy(comm_pca[[4]])
   ),
 
 
@@ -140,42 +140,43 @@ analysis_plan <- list(
   ),
 
 
-  # dN15 trait analysis
-
-  # prep data
-  tar_target(
-    name = trait_mean_dN15,
-    command = trait_mean |>
-      select(Gradient:mean, GS) |>
-      pivot_wider(names_from = trait_trans, values_from = mean) |>
-      pivot_longer(cols = c(Plant_Height_cm_log:dC13_permil), names_to = "trait_trans", values_to = "mean")
-  ),
+  ### SOIL dN15 TRAIT ANALYSIS
 
   # run model
   tar_target(
-    name = dN15_model,
-    command = run_dN15_model(dat = trait_mean_dN15,
+    name = dN15_trait_model,
+    command = run_trait_model(dat = trait_mean |>
+                               filter(!is.na(d15n)),
                               group = "trait_trans",
                               response = mean,
-                              continous_predictor = dN15_permil) |>
+                              continous_predictor = d15n) |>
         pivot_longer(cols = -c(trait_trans, data),
                      names_sep = "_",
-                     names_to = c(".value", "names")) |>
-        # remove models that have singular fit
-        filter(singular == FALSE)
+                     names_to = c(".value", "names"))
+  ),
+
+  # select best model
+  tar_target(
+    name = dN15_model,
+    command = dN15_trait_model |>
+      # remove models that have singular fit
+      filter(singular == FALSE) |>
+      filter(aic == min(aic))
   ),
 
   # Likelihood ratio test
   tar_target(
     name = dN15_lrt,
-    command = trait_mean_dN15 |>
+    command = trait_mean |>
+      filter(!is.na(d15n)) |>
+      filter(trait_trans == "LDMC") |>
       group_by(trait_trans) |>
       nest() |>
       mutate(LTR = map(data, ~{
-        ExN = lmer(mean ~  Gradient * dN15_permil + (1|GS), REML=FALSE, data = .x)
-        EplusN = lmer(mean ~  Gradient + dN15_permil + (1|GS), REML=FALSE, data = .x)
+        ExN = lmer(mean ~  Gradient * poly(d15n, 2) + (1|GS), REML=FALSE, data = .x)
+        EplusN = lmer(mean ~  Gradient + poly(d15n, 2) + (1|GS), REML=FALSE, data = .x)
         N = lmer(mean ~  Gradient + (1|GS), REML=FALSE, data = .x)
-        E = lmer(mean ~  dN15_permil + (1|GS), REML=FALSE, data = .x)
+        E = lmer(mean ~  poly(d15n, 2) + (1|GS), REML=FALSE, data = .x)
         Null = lmer(mean ~  1 + (1|GS), REML=FALSE, data = .x)
         # lr test
         test = anova(ExN, EplusN, N, E, Null)
@@ -189,10 +190,11 @@ analysis_plan <- list(
     name = dN15_model_output,
     command = model_output_prediction(dN15_model) |>
       # add LRT text
-      mutate(text = case_when(trait_trans %in% c("Plant_Height_cm_log", "C_percent") ~ "NxδN15",
-                              trait_trans %in% c("Dry_Mass_g_log", "Leaf_Area_cm2_log", "Thickness_mm_log", "LDMC") ~ "N+δN15",
-                              trait_trans %in% c("N_percent", "CN_ratio") ~ "δN15",
-                              TRUE ~ "Null"))
+      mutate(text = case_when(trait_trans %in% c("NP_ratio") ~ "NxδN15",
+                              trait_trans %in% c("dN15_permil") ~ "N+δN15",
+                              trait_trans %in% c("N_percent", "CN_ratio", "dC13_permil") ~ "N",
+                              trait_trans %in% c("Plant_Height_cm_log", "Dry_Mass_g_log") ~ "Null",
+                              TRUE ~ "δN15"))
   ),
 
   # regression model output
@@ -208,8 +210,12 @@ analysis_plan <- list(
       mutate(term = recode(term,
                            "(Intercept)" = "Intercept",
                            "GradientB" = "N",
-                           ".continous_predictor" = "δN15",
-                           "GradientB:.continous_predictor" = "NxδN15")) %>%
+                           ".continous_predictor" = "E",
+                           "GradientB:.continous_predictor" = "NxE" ,
+                           "poly(.continous_predictor, 2)1" = "E",
+                           "poly(.continous_predictor, 2)2" = "E2",
+                           "GradientB:poly(.continous_predictor, 2)1" = "NxE",
+                           "GradientB:poly(.continous_predictor, 2)2" = "NxE2")) %>%
       # join R squared
       left_join(dN15_model_output |>
                   select(-data, -mod, -singular, -prediction, -model_output) |>
@@ -228,53 +234,15 @@ analysis_plan <- list(
       write_csv(., file = "output/dN15_trait_regression_output.csv")
   ),
 
-
-
-  # # Community trait variance
-  # # run linear and quadratic model
-  # # NP ratio, both models are singular fit
-  # tar_target(
-  #   name = trait_variance_model,
-  #   command = run_trait_model(dat = trait_mean,
-  #                             group = "trait_trans",
-  #                             response = var,
-  #                             continous_predictor = Elevation_m) |>
-  #     pivot_longer(cols = -c(trait_trans, data),
-  #                  names_sep = "_",
-  #                  names_to = c(".value", "names"))
-  # ),
-  #
-  # # select best model
-  # tar_target(
-  #   name = community_variance_model,
-  #   command = trait_variance_model |>
-  #     # remove models that have singular fit
-  #     filter(singular == FALSE) |>
-  #     filter(aic == min(aic))
-  # ),
-  #
-  # # LRT for variance
-  # tar_target(
-  #   name = ltr_variance,
-  #   command = likelihood_ratio_test_variance(trait_mean |>
-  #                                              filter(trait_trans != "NP_ratio"))
-  # ),
-  #
-  # # Produce model output and prediction
-  # tar_target(
-  #   name = community_variance_output,
-  #   command = model_output_prediction(community_variance_model) |>
-  #     # add LRT text
-  #     mutate(text = case_when(trait_trans %in% c("SLA_cm2_g", "dC13_permil") ~ "Null",
-  #                             trait_trans %in% c("Plant_Height_cm_log") ~ "E",
-  #                             TRUE ~ "NxE"))
-  # ),
-  #
-  # # trait table
-  # tar_target(
-  #   name = trait_variance_table,
-  #   command = make_trait_variance_table(community_variance_output)
-  # ),
+  # correlation between leaf and soil dN15
+  tar_target(
+    name = dN15_cor,
+    command = trait_mean |>
+      filter(trait_trans == "dN15_permil",
+             !is.na(d15n)) |>
+      group_by(Gradient) |>
+      summarise(cor_dn15 = cor(mean, d15n))
+  ),
 
 
   # MULTIVARIATE TRAIT ANALYSIS
@@ -291,7 +259,7 @@ analysis_plan <- list(
     command = {
 
       trait_pca[[2]] %>%
-        filter(class != "Environment") |>
+        filter(class != "Soil abiotics") |>
         select(Trait = trait_fancy, PC1:PC4) %>%
         mutate(PC1 = round(PC1, digits = 2),
                PC2 = round(PC2, digits = 2),
@@ -314,114 +282,65 @@ analysis_plan <- list(
 
   tar_target(
     name = adonis_trait_output,
-    command = tidy(trait_pca[[4]]$aov.tab)
+    command = tidy(trait_pca[[4]])
     ),
 
 
   # test PCA1 regression
   # run linear and quadratic model
-  tar_target(
-    name = PCA1_community_model,
-    command = {
-
-      dat <- trait_pca[[1]] |>
-        rename(Elevation_m = Mean_elevation)
-
-      mod_linear <-  lmer(PC1 ~  Gradient * Elevation_m + (1|GS), data = dat)
-      mod_quadratic <-  lmer(PC1 ~  Gradient * poly(Elevation_m, 2) + (1|GS), data = dat)
-      AIC(mod_linear, mod_quadratic)
-
-      ExN = lmer(PC1 ~  Gradient * poly(Elevation_m, 2) + (1|GS), REML=FALSE, data = dat)
-      EplusN = lmer(PC1 ~  Gradient + poly(Elevation_m, 2) + (1|GS), REML=FALSE, data = dat)
-      N = lmer(PC1 ~  Gradient + (1|GS), REML=FALSE, data = dat)
-      E = lmer(PC1 ~  poly(Elevation_m, 2) + (1|GS), REML=FALSE, data = dat)
-      Null = lmer(PC1 ~  1 + (1|GS), REML=FALSE, data = dat)
-      # lr test
-      test = anova(ExN, EplusN, N, E, Null)
-
-
-      r_squared <- r.squaredGLMM(mod_quadratic) |>
-        as_tibble()
-
-      # best model is NxE
-      tidy(mod_quadratic) |>
-        filter(effect == "fixed") |>
-        mutate(term = recode(term,
-                             "(Intercept)" = "Intercept",
-                             "GradientB" = "N",
-                             "Elevation_m" = "E",
-                             "GradientB:.continous_predictor" = "NxE" ,
-                             "poly(Elevation_m, 2)1" = "E",
-                             "poly(Elevation_m, 2)2" = "E2",
-                             "GradientB:poly(Elevation_m, 2)1" = "NxE",
-                             "GradientB:poly(Elevation_m, 2)2" = "NxE2")) |>
-        bind_cols(r_squared) |>
-        mutate(estimate = round(estimate, digits = 2),
-               std.error = round(std.error, digits = 2),
-               statistic = round(statistic, digits = 2),
-               R2m = round(R2m, digits = 2),
-               R2c = round(R2c, digits = 2)) %>%
-        ungroup() |>
-        select(Term = term, Estimate = estimate, "Std error" = std.error, "t-value" = "statistic", "Marginal R2" = R2m, "Conditional R2" = R2c) %>%
-        write_csv(., file = "output/PCA_regression_output.csv")
-
-      }
-  ),
+  # tar_target(
+  #   name = PCA1_community_model,
+  #   command = {
+  #
+  #     dat <- trait_pca[[1]] |>
+  #       rename(Elevation_m = Mean_elevation)
+  #
+  #     mod_linear <-  lmer(PC1 ~  Gradient * Elevation_m + (1|GS), data = dat)
+  #     mod_quadratic <-  lmer(PC1 ~  Gradient * poly(Elevation_m, 2) + (1|GS), data = dat)
+  #     AIC(mod_linear, mod_quadratic)
+  #
+  #     ExN = lmer(PC1 ~  Gradient * poly(Elevation_m, 2) + (1|GS), REML=FALSE, data = dat)
+  #     EplusN = lmer(PC1 ~  Gradient + poly(Elevation_m, 2) + (1|GS), REML=FALSE, data = dat)
+  #     N = lmer(PC1 ~  Gradient + (1|GS), REML=FALSE, data = dat)
+  #     E = lmer(PC1 ~  poly(Elevation_m, 2) + (1|GS), REML=FALSE, data = dat)
+  #     Null = lmer(PC1 ~  1 + (1|GS), REML=FALSE, data = dat)
+  #     # lr test
+  #     test = anova(ExN, EplusN, N, E, Null)
+  #
+  #
+  #     r_squared <- r.squaredGLMM(mod_quadratic) |>
+  #       as_tibble()
+  #
+  #     # best model is NxE
+  #     tidy(mod_quadratic) |>
+  #       filter(effect == "fixed") |>
+  #       mutate(term = recode(term,
+  #                            "(Intercept)" = "Intercept",
+  #                            "GradientB" = "N",
+  #                            "Elevation_m" = "E",
+  #                            "GradientB:.continous_predictor" = "NxE" ,
+  #                            "poly(Elevation_m, 2)1" = "E",
+  #                            "poly(Elevation_m, 2)2" = "E2",
+  #                            "GradientB:poly(Elevation_m, 2)1" = "NxE",
+  #                            "GradientB:poly(Elevation_m, 2)2" = "NxE2")) |>
+  #       bind_cols(r_squared) |>
+  #       mutate(estimate = round(estimate, digits = 2),
+  #              std.error = round(std.error, digits = 2),
+  #              statistic = round(statistic, digits = 2),
+  #              R2m = round(R2m, digits = 2),
+  #              R2c = round(R2c, digits = 2)) %>%
+  #       ungroup() |>
+  #       select(Term = term, Estimate = estimate, "Std error" = std.error, "t-value" = "statistic", "Marginal R2" = R2m, "Conditional R2" = R2c) %>%
+  #       write_csv(., file = "output/PCA_regression_output.csv")
+  #
+  #     }
+  # ),
 
 
   ### ITV ANALYSIS
   tar_target(
     name = itv_output,
     command = make_ITV_analysis(trait_mean))
-
-
-  # # BRYOPHYTES
-  # # combine vascular and bryophyte data
-  # tar_target(
-  #   name = ind_traits,
-  #   command = combine_traits(traits_raw, bryo_traits_raw)),
-  #
-  #
-  # # BRYOPHYTES (maybe remove?)
-  # # run linear and quadratic model
-  # tar_target(
-  #   name = trait_bryophyte_model,
-  #   command = run_bryophyte_model(dat = ind_traits |>
-  #                               filter(Functional_group == "bryophyte"),
-  #                             group = c("trait_trans", "Taxon"),
-  #                             response = Value,
-  #                             continous_predictor = Elevation_m) |>
-  #     pivot_longer(cols = -c(Taxon, trait_trans, data),
-  #                  names_sep = "_",
-  #                  names_to = c(".value", "names"))
-  # ),
-  #
-  # # select best model
-  # tar_target(
-  #   name = bryophyte_model,
-  #   command = trait_bryophyte_model |>
-  #     filter(aic == min(aic))
-  # ),
-  #
-  # # Produce model output and prediction
-  # tar_target(
-  #   name = bryophyte_model_output,
-  #   command = bryophyte_model |>
-  #     # make model output and prediction
-  #     mutate(model_output = map(mod, tidy),
-  #            prediction = map2(.x = mod, .y = data,
-  #                              .f = ~augment(.x, interval = "confidence") |>
-  #                                select(.fitted, .lower, .upper) |>
-  #                                bind_cols(.y))) #|>
-  #     #unnest(prediction)
-  # ),
-  #
-  # # Produce model output and prediction
-  # tar_target(
-  #   name = bryo_table,
-  #   command = make_bryo_table(bryophyte_model_output)
-  # )
-
 
 )
 
