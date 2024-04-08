be412c82a1e6ad31b114deb947ecccdde892505d
audhalbritter <me@audhalbritter.com>
2024-04-08 14:26:39 +0200

    download isotope data

--- R/download_plan.R
+++ R/download_plan.R
@@ -50,5 +50,15 @@ download_plan <- list(
                        path = "clean_data/soil",
                        remote_path = "Soil"),
     format = "file"
+  ),
+
+  # soil data
+  tar_target(
+    name = soil_isotopes,
+    command = get_file(node = "smbqh",
+                       file = "PFTC4_Svalbard_2022_Gradient_Clean_Soil_Isotopes.xlsx",
+                       path = "clean_data/soil",
+                       remote_path = "RawData/RawData_Soil"),
+    format = "file"
   )
 )
